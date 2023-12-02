ec0b1a90b77e4fbc02464db25bacfb38cb9567f6
Duke Lasagna <47915737+DukeLasagna@users.noreply.github.com>
2023-12-03 01:48:02 +0800

    aura with cool down and rng time activate

--- Biblically Accurate Game/Assets/Scripts/tempboss_weapon.cs
+++ Biblically Accurate Game/Assets/Scripts/tempboss_weapon.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class tempboss_weapon : MonoBehaviour
 {   
-    public GameObject je_sus;
+   
     public GameObject poopoo;
 
     //public Transform firePoint;
@@ -19,15 +19,25 @@ public class tempboss_weapon : MonoBehaviour
     public float rotationModifier;
 
     System.Random rngatk = new System.Random((int)DateTime.Now.Ticks);
+    public GameObject aura;
     private int atk = 0;
     private int timer = 0; // Initial timer duration
-    private int duration = 0;
+    public int duration = 0;
     public int cooldown = 0;
     private bool isTimerRunning = false;
+    private bool attacking = false;
 
+    public int aura_duration = 5;
+    public int debug_counter = 0;
+    public int aura_cooldown = 0;   
+    public int aura_cooling = 0;
+    private bool aura_on = false;
     private void Start()
     {
         StartTimer();
+        //make aura invisible
+        aura.GetComponent<Renderer>().enabled = false;
+        aura_cooldown = rngatk.Next(5, 20);
     }
 
     private void Update()
@@ -50,28 +60,54 @@ public class tempboss_weapon : MonoBehaviour
 
         }
 
-
         atk = rngatk.Next(0, 3);
         cooldown = rngatk.Next(5, 20);
 
-        if (duration == cooldown)
-        {
+       
             if (atk == 0)
-            {
+            {   /*
+                attacking = true;
                 Debug.Log("poopoo go!");
                 GameObject projectile1 = Instantiate(poopoo, firepoint.position, firepoint.rotation);
                 projectile1.GetComponent<Rigidbody2D>().AddForce(firepoint.up * boss_fireforce, ForceMode2D.Impulse);
                 cooldown = 0;
-            }
-            else if (atk == 1)
-            {
-                Debug.Log("je_sus go!");
-                GameObject projectile2 = Instantiate(je_sus, firepoint.position, firepoint.rotation);
-                projectile2.GetComponent<Rigidbody2D>().AddForce(firepoint.up * boss_fireforce, ForceMode2D.Impulse);
-                cooldown = 0;
+                attacking = false;
+                */
             }
 
-        }
+            else if ( atk == 1 || aura_on )
+            {   
+                if(aura_cooldown <= aura_cooling) 
+                { 
+                    if(attacking == false)
+                    {
+                        duration = 0;
+                    }
+                    Debug.Log("aura is here!");
+                    if (duration < aura_duration)
+                    {   
+                        aura_on = true;
+                        attacking = true;
+                        aura.GetComponent<Renderer>().enabled = true;
+                        Debug.Log("aura go!");
+                    
+                    }
+                    else 
+                    {   
+                        Debug.Log("aura gone!");
+                        aura.GetComponent<Renderer>().enabled = false;
+                        aura_cooldown = rngatk.Next(5, 20);
+                        attacking = false;
+                        aura_cooling = 0;
+                        aura_on = false;
+                        duration = 0;
+                    }
+                }
+                
+            }
+
+
+        
 
     }
 
@@ -91,6 +127,7 @@ public class tempboss_weapon : MonoBehaviour
             yield return new WaitForSeconds(1f); // Wait for 1 second
             timer++;
             duration++;
+            aura_cooling++;
 
             // You can do something each second (e.g., update a display)
             Debug.Log("Time elapsed: " + timer + " seconds");
