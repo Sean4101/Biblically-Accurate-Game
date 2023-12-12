8abfc31a3780da587a7bf8f448516f1e4cb6c615
Duke Lasagna <47915737+DukeLasagna@users.noreply.github.com>
2023-12-12 18:29:54 +0800

    better dialogue system, avatar able to present

--- Biblically Accurate Game/Assets/Scripts/Dialogue System/DialogLines.cs
+++ Biblically Accurate Game/Assets/Scripts/Dialogue System/DialogLines.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public class DialogLine
+{
+    public string name;
+    public string line;
+    public Sprite avatar;
+}
+
 [CreateAssetMenu(fileName = "New Dialog", menuName = "Scriptable Object/Dialog")]
 public class DialogLines : ScriptableObject
 {
-    public List<string> lines;
+    public List<DialogLine> lines;
 }
