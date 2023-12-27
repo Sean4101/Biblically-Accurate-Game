e242fe0bb702fefc0a3875b17861363071948bfe
Duke Lasagna <47915737+DukeLasagna@users.noreply.github.com>
2023-12-27 16:34:45 +0800

    Update BulletTimeManager.cs

--- Biblically Accurate Game/Assets/BulletTimeManager.cs
+++ Biblically Accurate Game/Assets/BulletTimeManager.cs
@@ -6,12 +6,24 @@ public class BulletTimeManager : MonoBehaviour
 {   
     public float slowdownFactor = 0.001f;
     public float slowdownLength = 10f;
+    public float timeLeft;
+
+    private void Start()
+    {
+        timeLeft = slowdownLength;
+    }
 
     void Update()
-    {
+    {   
+        Debug.Log(timeLeft);
+        timeLeft -= Time.deltaTime;
         Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        if (timeLeft <= 0)
+        {
+            timeLeft = slowdownLength;
+        }
     }
     public void DoSlowMotion()
     {
